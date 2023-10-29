8c0d763373eb8c1f0f1a7acd9dd762e6ab2f173c
hk-117 <zubayer11704@gmail.com>
2023-10-29 20:11:54 +0600

    markdown support to notes

--- src/components/Pages/Note/NoteEditor.js
+++ src/components/Pages/Note/NoteEditor.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React,{useState} from 'react';
+import Markdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
 import Layout from '../../Layouts/Layout';
 
 export default function NoteEditor(props){
+    const [title,setTitle] = useState("");
+    const [content,setContent] = useState("");
+
+    function changeTitleHandler(e){
+        setTitle(e.target.value);
+    }
+
+    function changeContentHandler(e){
+        setContent(e.target.value);
+    }
+
     return(
         <Layout>
             <div className="container vh-90 mt-3">
@@ -10,19 +23,23 @@ export default function NoteEditor(props){
                         <form>
                             <div className="mb-3">
                                 <label htmlFor="noteTitle" className="form-label">Title</label>
-                                <input type="text" className="form-control" id="noteTitle"/>
+                                <input type="text" className="form-control" id="noteTitle" value={title} onChange={changeTitleHandler}/>
                             </div>
                             <div className="mb-3">
                                 <label htmlFor="noteContent" className="form-label">Note</label>
-                                <textarea className="form-control" id="noteContent" rows="25">
+                                <textarea className="form-control" id="noteContent" rows="25" value={content} onChange={changeContentHandler}>
                                     
                                 </textarea>
                             </div>
                         </form>
                     </div>
                     <div className='col-md-6 border rounded-end  p-3'>
-                        <h1>Output: </h1>
-                        <div id="output-div"></div>
+                        <h1> {title} </h1>
+                        <div id="output-div" className='text-break'>
+                            <Markdown remarkPlugins={[remarkGfm]}>
+                                {content}
+                            </Markdown>
+                        </div>
                     </div>
                 </div>
             </div>
